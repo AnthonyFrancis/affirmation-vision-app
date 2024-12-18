738fede2e4f980a2a6fc2fbcc3038ca45defbcda
AnthonyFrancis <tonytrack@gmail.com>
2024-12-18 15:08:41 +0000

    Initial Commit

---
+++ visionboard/visionboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionboardApp.swift
+//  visionboard
+//
+//  Created by Anthony Francis on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct visionboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ visionboardTests/visionboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  visionboardTests.swift
+//  visionboardTests
+//
+//  Created by Anthony Francis on 18/12/2024.
+//
+
+import Testing
+@testable import visionboard
+
+struct visionboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
